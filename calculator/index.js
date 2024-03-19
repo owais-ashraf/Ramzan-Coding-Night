57288a92678f1a567dc92b1248a56f3249135248
Ozil <65316921+owais-ashraf@users.noreply.github.com>
2024-03-20 05:07:17 +0530

    Add files via upload

--- calculator/index.js
+++ calculator/index.js
@@ -1,4 +1,6 @@
-import inquirer from "inquirer";
+#! /usr/bin/env node  
+// 👆 is line ko SHABANG bh bolte hain, hashtag ka sha aur bang exclamation mark ko bhi bolte hain
+import inquirer from 'inquirer';
 const answer = await inquirer.prompt([
     { message: "Enter First Number: ", type: "number", name: "FirstNumber" },
     { message: "Enter Second Number: ", type: "number", name: "SecondNumber" },
